745daec163b77b2689739d5391fdadeb3b571224
ahirepankaj <pankaj.ninja@gmail.com>
2020-05-29 22:22:26 +0530

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let deferredPrompt;
-const addBtn = document.querySelector('add-button');
+const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
 
 window.addEventListener('beforeinstallprompt', (e) => {
