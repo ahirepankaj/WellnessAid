b727c9d020a748e31cd46d7cf7b976628a22c5a6
ahirepankaj <pankaj.ninja@gmail.com>
2020-05-29 20:20:37 +0530

    updates for pwa
    updates for pwa

---
+++ pwabuilder-sw.js
@@ -0,0 +1,26 @@
+// This is the "Offline copy of assets" service worker
+
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
+
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  event.respondWith(cacheFirst(req));
+});
+
+
