81fb05b39d1e51e9bc35573dcbbfc6a186002a95
ahirepankaj <pankaj.ninja@gmail.com>
2020-05-30 11:51:28 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,10 +1,10 @@
-let deferredPrompt;
+let deferredPrompt1;
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent the mini-infobar from appearing on mobile
   e.preventDefault();
   // Stash the event so it can be triggered later.
-  deferredPrompt = e;
+  deferredPrompt1 = e;
   // Update UI notify the user they can install the PWA
   showInstallPromotion();
 });
