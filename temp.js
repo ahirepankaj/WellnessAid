c74f6bbabf6a2db4e5b585c30e1cc5aba274b288
ahirepankaj <pankaj.ninja@gmail.com>
2020-05-30 12:01:39 +0530

    Update temp.js

--- temp.js
+++ temp.js
@@ -6,5 +6,16 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt1 = e;
   // Update UI notify the user they can install the PWA
-  showInstallPromotion();
+
+  
+  deferredPrompt1.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt1.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+        deferredPrompt1 = null;
+      });
 });
